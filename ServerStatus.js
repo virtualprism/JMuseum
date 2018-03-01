aafe3cd2d834aeb2a43287843400f3ef4219d513
PrismRealm <alan159753@gmail.com>
2018-03-01 10:48:07 +0800

    Change 在 ServerStatus.js 中的讀取與回存函式，多了Promise的選擇與錯誤防範。

--- ServerStatus.js
+++ ServerStatus.js
@@ -2,23 +2,51 @@ const fileSystem = require("fs");
 
 /**
  * 讀取伺服器狀態檔案。
- * @param {CallbackFunction} callback 回呼函式。
+ * @param {CallbackFunction?} callback 回呼函式。
+ * @throws {TypeError} 若傳入callback的參數不為Function或undefined，則擲出「型別錯誤」。
+ * @return {undefined | Promise} 若沒有傳入回呼函式，則會回傳Promise。
  */
 function LoadStatus(callback) {
-    fileSystem.readFile("status.json", {encoding: "utf8" }, (err, data) => {
-        if (err) return callback(err, false);
-        module.exports.status = JSON.parse(data);
-        callback(null, true);
-    });
+    if (callback instanceof Function) {
+        fileSystem.readFile("status.json", {encoding: "utf8" }, (err, data) => {
+            if (err) return callback(err, false);
+            module.exports.status = JSON.parse(data);
+            callback(null, true);
+        });
+    }
+    else if (callback === undefined) {
+        return new Promise((res, rej) => {
+            fileSystem.readFile("status.json", {encoding: "utf8" }, (err, data) => {
+                if (err) return rej(err);
+                module.exports.status = JSON.parse(data);
+                res(true);
+            });
+        })
+    }
+    else {
+        throw new TypeError("First argument must be callback function or undefined.");
+    }
 }
 
 /**
  * 儲存伺服器狀態檔案。
- * @param {CallbackFunction} callback 回呼函式。
+ * @param {CallbackFunction?} callback 回呼函式。
+ * @throws {TypeError} 若傳入callback的參數不為Function或undefined，則擲出「型別錯誤」。
+ * @return {undefined | Promise} 若沒有傳入回呼函式，則會回傳Promise。
  */
 function SaveStatus(callback) {
     let data = module.exports.status;
-    fileSystem.writeFile("status.json", JSON.stringify(data), { encoding: "utf8" }, callback);
+    if (callback instanceof Function) {
+        fileSystem.writeFile("status.json", JSON.stringify(data), { encoding: "utf8" }, callback);
+    }
+    else if (callback === undefined) {
+        return new Promise((res, rej) => {
+            fileSystem.writeFile("status.json", JSON.stringify(data), { encoding: "utf8" }, err => err ? rej(err) : res());
+        });
+    }
+    else {
+        throw new TypeError("First argument must be callback function or undefined.");
+    }
 }
 
 module.exports.LoadStatus = LoadStatus;
